466b66a945bdb3e44a1f984adb7ae3548babe7a8
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-02-26 22:24:34 -0500

    Added the ability to make three calls to my char gen endpoint in order to get three random char options. Next is to send that info to an endpoint to create a Character. Also to login and tie the choice and character created to the User.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="hello">
-    <h1>{{ info }}</h1>
-    <button @click="showOrigin()">Show Origins</button>
+    <div class="col-one">
+      <h2>{{ origin1}}</h2>
+      <h2>{{ origin2}}</h2>
+      <button id="combo-1">Create Character</button>
+    </div>
+
+    <div class="col-two">
+      <h2>{{ origin3}}</h2>
+      <h2>{{ origin4}}</h2>
+      <button id="combo-2">Create Character</button>
+    </div>
+
+    <div class="col-three">
+      <h2>{{ origin5}}</h2>
+      <h2>{{ origin6}}</h2>
+      <button id="combo-3">Create Character</button>
+    </div>
+
+    <div class="btn">
+      <br>
+      <button @click="showChoice1(), showChoice2(), showChoice3()">Show Origins</button>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+
 export default {
   name: "HelloWorld",
   props: {
@@ -14,11 +35,19 @@ export default {
   },
   data() {
     return {
-      info: ""
+      info: "",
+      info2: "",
+      info3: "",
+      origin1: "",
+      origin2: "",
+      origin3: "",
+      origin4: "",
+      origin5: "",
+      origin6: ""
     };
   },
   methods: {
-    async showOrigin() {
+    async showChoice1() {
       let config = {
         headers: {
           Accept: "application/json"
@@ -26,11 +55,54 @@ export default {
       };
       const info = await axios.get(
         "http://0.0.0.0:8000/api/quickChar/",
-        config
+        config,
+        {
+          useCredentails: false
+        }
       );
 
       this.info = info.data;
-      console.log(info.data);
+      this.origin1 = info.data[0].origin;
+      this.origin2 = info.data[1].origin;
+      console.log(this.info);
+    },
+    async showChoice2() {
+      let config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+      const info = await axios.get(
+        "http://0.0.0.0:8000/api/quickChar/",
+        config,
+        {
+          useCredentails: false
+        }
+      );
+
+      this.info2 = info.data;
+      this.origin3 = info.data[0].origin;
+      this.origin4 = info.data[1].origin;
+      console.log(this.info2);
+    },
+    async showChoice3() {
+      let config = {
+        headers: {
+          Accept: "application/json"
+        }
+      };
+      const info = await axios.get(
+        "http://0.0.0.0:8000/api/quickChar/",
+        config,
+        {
+          useCredentails: false
+        }
+      );
+
+      this.info3 = info.data;
+      this.origin5 = info.data[0].origin;
+      this.origin6 = info.data[1].origin;
+      console.log(this.info3);
     }
   }
 };
@@ -51,5 +123,21 @@ li {
 }
 a {
   color: #42b983;
+}
+.hello {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+
+.col-two {
+  grid-column: 2/3;
+}
+
+.col-three {
+  grid-column: 3/3;
+}
+
+.btn {
+  grid-column: 2/3;
 }
 </style>
