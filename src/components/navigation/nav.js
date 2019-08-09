e87aca18ff38e53a2c1215b41150d5a5ca42f81d
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-08-09 09:13:15 -0400

    Logout issue resolved.

--- src/components/navigation/nav.js
+++ src/components/navigation/nav.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function logout() {
-  let url = "http://0.0.0.0:8000/rest-auth/logout/";
+  let url = "http://localhost:8000/rest-auth/logout/";
   await axios.post(url).then(response => {
     this.$emit("showNavBar", false);
     console.log(response.data.detail);
